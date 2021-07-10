882f24951a5215e4e18e8a55a2e582faedf4d3a7
Kate Bradshaw <kaitlynmbradshaw@gmail.com>
2021-07-10 14:04:02 -0500

    user login, makes user be logged in before accessing back end, styles user login forms

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
   	@pending_customers = Customer.all.where(:status => "pending")
-
+  	@decision_customers = Customer.all.where.not(:status => "pending")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  authenticated :user do
+    root to: 'welcome#index', as: :authenticated_root
+    resources :customers
+  end
   get 'welcome/index'
-  root 'welcome#index'
-  resources :customers
+  root to: redirect('/users/sign_in')
+  resources :customers, only: [:new]
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
