b5c297fdf2cb741e50e50e69e9dc3eb7c9b2e327
Kate Bradshaw <kaitlynmbradshaw@gmail.com>
2021-07-15 17:31:23 -0500

    changes send to environmental variable

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -2,6 +2,6 @@ class CustomerMailer < ApplicationMailer
   def new_customer_email
     @customer = params[:customer]
 
-    mail(to: 'kaitlynmbradshaw@gmail.com', subject: "You got a new application!")
+    mail(to: ENV['SEND_TO_GMAIL'], subject: "You got a new application!")
   end
 end
