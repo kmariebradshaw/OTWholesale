485bb31e5b511d18de32ab29f6b0b962f57c2857
Kate Bradshaw <kaitlynmbradshaw@gmail.com>
2021-07-21 21:20:22 -0500

    updates local storage, url name, and adds requirements to form

--- db/schema.rb
+++ db/schema.rb
@@ -10,85 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_231316) do
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
+ActiveRecord::Schema.define(version: 2021_07_10_180302) do
 
   create_table "customers", force: :cascade do |t|
-    t.string "company"
+    t.string "company", null: false
     t.string "dba"
-    t.string "permit"
-    t.string "seller_permit_state"
+    t.string "permit", null: false
+    t.string "seller_permit_state", null: false
     t.integer "locations"
-    t.string "legal_status"
-    t.string "billing_addr"
-    t.string "billing_city"
-    t.string "billing_state"
-    t.string "billing_zip"
-    t.string "billing_ap"
+    t.string "legal_status", null: false
+    t.string "billing_addr", null: false
+    t.string "billing_city", null: false
+    t.string "billing_state", null: false
+    t.string "billing_zip", null: false
+    t.string "billing_ap", null: false
     t.string "billing_phone"
-    t.string "billing_email"
+    t.string "billing_email", null: false
     t.string "shipping_location"
     t.string "shipping_address"
     t.string "shipping_state"
     t.string "shipping_zip"
-    t.string "name"
-    t.string "email"
-    t.string "phone"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "phone", null: false
     t.string "top3_1"
     t.string "top3_2"
     t.string "top3_3"
     t.string "facebook"
     t.string "instagram"
-    t.boolean "agree1"
-    t.boolean "agree2"
+    t.boolean "agree1", null: false
+    t.boolean "agree2", null: false
     t.boolean "signed"
     t.string "referral_source"
     t.string "referral_source_other"
     t.string "status", default: "pending"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "photo_id"
-    t.string "photo_file_name"
-    t.string "photo_content_type"
-    t.integer "photo_file_size"
-    t.datetime "photo_updated_at"
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -105,6 +62,4 @@ ActiveRecord::Schema.define(version: 2021_07_11_231316) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
