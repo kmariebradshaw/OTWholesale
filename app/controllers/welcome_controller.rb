51855ea9802c25cf3bd55284bce0cf3080633543
Kate Bradshaw <kaitlynmbradshaw@gmail.com>
2021-08-03 06:59:51 -0500

    updates the style of the edit page and starts the welcome show functionality of a search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
+	skip_before_action :authenticate_user!, only: [:show]
+
   def index
   	@pending_customers = Customer.all.where(:status => "pending")
   	@decision_customers = Customer.all.where.not(:status => "pending")
   end
+  def show
+  	@employee = Employee.new 
+  end 
 end
