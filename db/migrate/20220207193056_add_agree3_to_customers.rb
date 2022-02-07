ceeed30abec32be10375acdfcc06973ac26e8443
Kate Bradshaw <kaitlynmbradshaw@gmail.com>
2022-02-07 13:46:21 -0600

    updates null req

--- db/migrate/20220207193056_add_agree3_to_customers.rb
+++ db/migrate/20220207193056_add_agree3_to_customers.rb
@@ -1,5 +1,5 @@
 class AddAgree3ToCustomers < ActiveRecord::Migration[6.1]
   def change
-  	 add_column :customers, :agree3, :boolean, :null false
+  	 add_column :customers, :agree3, :boolean
   end
 end
