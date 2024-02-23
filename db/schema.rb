7e574de81262d5e0799a8aca1df1ec0529d86f2d
Kaitlyn Bradshaw <kmbradshaw@ymail.com>
2024-02-23 17:55:47 -0600

    adds admin to login and creates permanent repush to Shopify option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_193056) do
+ActiveRecord::Schema.define(version: 2024_02_23_234554) do
 
   create_table "customers", force: :cascade do |t|
     t.string "company", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2022_02_07_193056) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "approved", default: false, null: false
+    t.boolean "admin"
     t.index ["approved"], name: "index_users_on_approved"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
